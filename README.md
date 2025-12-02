8d721b81fc0329756063504c2d03bdb002ba9d0f
Jared Komoroski <jkomoroski@alianza.com>
2025-12-02 12:18:46 -0700

    Add File init

--- README.md
+++ README.md
@@ -8,8 +8,9 @@ These are my solutions to [Advent of Code 2025](https://adventofcode.com/2025) u
 - maven
 
 ### How To
-- Create and add your inputs to `inputs/day-0x/in.txt`
-- Create and add your test inputs to `inputs/day-0x/test.txt`
 - Build with `./mvnw clean install`
 - Run with`java -jar target/aoc-2025-0.0.1-SNAPSHOT.jar 02`
   - Where `02` is a part of the name of the class you want to run
+  - On first run it will create the `inputs` tree
+- Update your test data to `inputs/day-0x/test.txt`
+- Update your input data to `./inputs/day-0x/in.txt`
--- src/main/java/ski/komoro/aoc/twentyfive/Main.java
+++ src/main/java/ski/komoro/aoc/twentyfive/Main.java
@@ -1,12 +1,18 @@
 package ski.komoro.aoc.twentyfive;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Main {
 
     static void main(String[] args) throws Exception {
+        initInputs(args);
         buildInstance(findDay(args)).run();
     }
 
@@ -26,9 +32,26 @@ public class Main {
         return Arrays.stream(AOCBase.class.getPermittedSubclasses())
                 .sorted(Comparator.comparing(Class::getSimpleName, Comparator.reverseOrder()))
                 .map(c -> (Class<? extends AOCBase>) c);
-
     }
 
+    static void initInputs(String[] args) throws IOException {
+        final Path inputs = Path.of("./inputs");
+        if(Files.exists(inputs)) {
+            return;
+        }
+        Files.createDirectory(inputs);
+        final List<String> suffixes = IntStream.rangeClosed(1, 12)
+                .mapToObj(i -> String.format("%02d", i))
+                .toList();
 
+        for (final String suffix : suffixes) {
+            final Path day = Path.of("./inputs/day-" + suffix);
+            final Path in = Path.of("./inputs/day-" + suffix + "/in.txt");
+            final Path test = Path.of("./inputs/day-" + suffix + "/test.txt");
+            Files.createDirectory(day);
+            Files.createFile(in);
+            Files.createFile(test);
+        }
+    }
 
 }
