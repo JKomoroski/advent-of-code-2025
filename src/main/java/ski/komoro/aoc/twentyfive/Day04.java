c7d462180ab8ed09bc1abfe9fd4ec736ff324ef2
Jared Komoroski <jkomoroski@alianza.com>
2025-12-04 08:33:40 -0700

    Solve Day 4

--- src/main/java/ski/komoro/aoc/twentyfive/Day04.java
+++ src/main/java/ski/komoro/aoc/twentyfive/Day04.java
@@ -1,6 +1,9 @@
 package ski.komoro.aoc.twentyfive;
 
+import java.util.List;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
+import ski.komoro.aoc.utils.Point2;
 
 public final class Day04 extends AOCBase {
 
@@ -11,11 +14,85 @@ public final class Day04 extends AOCBase {
 
     @Override
     protected String part1(final Stream<String> fileInput) throws Exception {
-        return "hello world";
+        char[][] grid = fileInput.map(String::toCharArray).toArray(char[][]::new);
+
+        final List<Point2> points = IntStream.range(0, grid.length)
+                .boxed()
+                .flatMap(y -> IntStream.range(0, grid[0].length).mapToObj(x -> new Point2(x, y)))
+                .toList();
+
+        int accessible = 0;
+
+        for (final var p : points) {
+            if(!isPaper(p, grid)) {
+                continue;
+            }
+            int neighborCount = 0;
+
+            for (final Point2 neighbor : p.neighbors()) {
+                if(isPaper(neighbor, grid)) {
+                    neighborCount++;
+                }
+            }
+
+            if(neighborCount < 4 ) {
+                accessible++;
+            }
+        }
+
+        return String.valueOf(accessible);
+    }
+
+    // Bound checking is for nerds
+    boolean isPaper(Point2 p, char[][] grid) {
+        try {
+            return grid[p.x()][p.y()] == '@';
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    void removePaper(Point2 p, char[][] grid) {
+        grid[p.x()][p.y()] = '.';
     }
 
     @Override
     protected String part2(final Stream<String> fileInput) throws Exception {
-        return "hello world";
+
+        char[][] grid = fileInput.map(String::toCharArray).toArray(char[][]::new);
+
+        final List<Point2> points = IntStream.range(0, grid.length)
+                .boxed()
+                .flatMap(y -> IntStream.range(0, grid[0].length).mapToObj(x -> new Point2(x, y)))
+                .toList();
+
+        boolean madeChange = true;
+        int removed = 0;
+
+        while (madeChange) {
+            madeChange = false;
+            for (final var p : points) {
+                if (!isPaper(p, grid)) {
+                    continue;
+                }
+                int neighborCount = 0;
+
+                for (final Point2 neighbor : p.neighbors()) {
+                    if (isPaper(neighbor, grid)) {
+                        neighborCount++;
+                    }
+                }
+
+                if (neighborCount < 4) {
+                    removePaper(p, grid);
+                    removed++;
+                    madeChange = true;
+                }
+            }
+        }
+
+        return String.valueOf(removed);
     }
+
+
 }
--- src/main/java/ski/komoro/aoc/utils/Point2.java
+++ src/main/java/ski/komoro/aoc/utils/Point2.java
@@ -1,5 +1,22 @@
 package ski.komoro.aoc.utils;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 public record Point2(int x, int y) {
+
+    // Bound checking is for nerds
+    public List<Point2> neighbors() {
+        return Stream.of(
+                new Point2(x, y - 1),
+                new Point2(x, y + 1),
+                new Point2(x - 1, y),
+                new Point2(x + 1, y),
+                new Point2(x - 1, y - 1),
+                new Point2(x + 1, y - 1),
+                new Point2(x - 1, y + 1),
+                new Point2(x + 1, y + 1)
+        ).toList();
+    }
 
 }
