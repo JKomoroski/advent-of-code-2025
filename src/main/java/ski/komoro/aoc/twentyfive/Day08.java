c67f058248bb2c75dd35d3e3f9cee33f255cefa3
Jared Komoroski <jkomoroski@alianza.com>
2025-12-08 11:04:51 -0700

    Day 8 refactor

--- src/main/java/ski/komoro/aoc/twentyfive/Day08.java
+++ src/main/java/ski/komoro/aoc/twentyfive/Day08.java
@@ -14,6 +14,7 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import ski.komoro.aoc.utils.Point3;
+import ski.komoro.aoc.utils.Tuple;
 
 public final class Day08 extends AOCBase {
 
@@ -28,58 +29,17 @@ public final class Day08 extends AOCBase {
                 .map(l -> new Point3(Long.parseLong(l[0]), Long.parseLong(l[1]), Long.parseLong(l[2])))
                 .toList();
 
-        Map<Point3, LinkedHashMap<Point3, Double>> data = list.stream()
-                .collect(Collectors.toMap(
-                        Function.identity(),
-                        p -> list.stream()
-                                .filter(not(p::equals))
-                                .collect(Collectors.toMap(Function.identity(), p2 -> p2.computeDistance(p)))
-                                .entrySet().stream()
-                                .sorted(Map.Entry.comparingByValue())
-                                .collect(Collectors.toMap(
-                                        Map.Entry::getKey,
-                                        Map.Entry::getValue,
-                                        (e1, e2) -> e1,
-                                        LinkedHashMap::new
-                                ))
-                        )
-                );
+        Map<Point3, LinkedHashMap<Point3, Double>> data = buildDistanceMap(list);
+
         List<Circuit> circuits = new ArrayList<>();
         int connections = 0;
-        int maxTry = list.size() == 20 ? 10 : 1000;
-        while (connections < maxTry && data.values().stream().anyMatch(m -> !m.isEmpty())) {
+        int maxTry = data.size() == 20 ? 10 : 1000;
+
+        while (connections < maxTry && hasUnconnectedPoints(data)) {
             connections++;
-            Point3 hasNearest = list.stream()
-                    .filter(p -> !data.get(p).isEmpty())
-                    .min(Comparator.comparingDouble(p -> nearestDistance(p, data)))
-                    .orElseThrow();
-            Point3 unconnected = nearestPoint(hasNearest, data);
-            removeNearestPoint(hasNearest, data);
-            Circuit c1 = circuits.stream()
-                    .filter(c -> c.hasPoint(hasNearest))
-                    .findFirst()
-                    .orElse(null);
-            Circuit c2 = circuits.stream()
-                    .filter(c -> c.hasPoint(unconnected))
-                    .findFirst()
-                    .orElse(null);
-            if (c1 == null && c2 == null) {
-                // Neither in circuit - create new
-                circuits.add(new Circuit(hasNearest, unconnected));
-            } else if (c1 != null && c2 != null) {
-                // Both in circuits - merge if different
-                if (c1 != c2) {
-                    c1.merge(c2);
-                    circuits.remove(c2);
-                }
-                // else: already connected, skip
-            } else if (c1 != null) {
-                // Only hasNearest in circuit
-                c1.addPoint(unconnected);
-            } else {
-                // Only unconnected in circuit
-                c2.addPoint(hasNearest);
-            }
+            var connection = findPointWithNearestNeighbor(data);
+            removeNearestPoint(connection.left(), data);
+            connectPoints(connection.left(), connection.right(), circuits);
         }
 
         var longest = circuits.stream()
@@ -90,7 +50,45 @@ public final class Day08 extends AOCBase {
         return String.valueOf(longest);
     }
 
+    @Override
+    protected String part2(final Stream<String> fileInput) throws Exception {
+        final List<Point3> list = fileInput.map(s -> s.split(","))
+                .map(l -> new Point3(Long.parseLong(l[0]), Long.parseLong(l[1]), Long.parseLong(l[2])))
+                .toList();
 
+        Map<Point3, LinkedHashMap<Point3, Double>> data = buildDistanceMap(list);
+
+        List<Circuit> circuits = new ArrayList<>();
+        Tuple<Point3, Point3> lastConnection = null;
+
+        while (hasUnconnectedPoints(data)) {
+            var connection = findPointWithNearestNeighbor(data);
+            removeNearestPoint(connection.left(), data);
+            connectPoints(connection.left(), connection.right(), circuits);
+            if (circuits.size() == 1 && circuits.getFirst().size() == data.size()) {
+                lastConnection = connection;
+                break;
+            }
+        }
+        if (lastConnection == null) {
+            throw new NullPointerException();
+        }
+
+        return String.valueOf(lastConnection.left().x() * lastConnection.right().x());
+    }
+
+    private Tuple<Point3, Point3> findPointWithNearestNeighbor(Map<Point3, LinkedHashMap<Point3, Double>> data) {
+        var p = data.keySet()
+                .stream()
+                .filter(p1 -> !data.get(p1).isEmpty())
+                .min(Comparator.comparingDouble(p1 -> nearestDistance(p1, data)))
+                .orElseThrow();
+        return Tuple.of(p, nearestPoint(p, data));
+    }
+
+    private boolean hasUnconnectedPoints(Map<Point3, LinkedHashMap<Point3, Double>> data) {
+        return data.values().stream().anyMatch(m -> !m.isEmpty());
+    }
 
     Point3 nearestPoint(Point3 point, Map<Point3, LinkedHashMap<Point3, Double>> data) {
         return data.get(point).firstEntry().getKey();
@@ -105,80 +103,52 @@ public final class Day08 extends AOCBase {
         data.get(nearest.getKey()).remove(point);
     }
 
-    @Override
-    protected String part2(final Stream<String> fileInput) throws Exception {
-        final List<Point3> list = fileInput.map(s -> s.split(","))
-                .map(l -> new Point3(Long.parseLong(l[0]), Long.parseLong(l[1]), Long.parseLong(l[2])))
-                .toList();
+    private Map<Point3, LinkedHashMap<Point3, Double>> buildDistanceMap(List<Point3> points) {
+        return points.stream()
+                .collect(Collectors.toMap(Function.identity(), point -> buildSortedDistances(point, points)));
+    }
 
-        Map<Point3, LinkedHashMap<Point3, Double>> data = list.stream()
-                .collect(Collectors.toMap(
-                                Function.identity(),
-                                p -> list.stream()
-                                        .filter(not(p::equals))
-                                        .collect(Collectors.toMap(Function.identity(), p2 -> p2.computeDistance(p)))
-                                        .entrySet().stream()
-                                        .sorted(Map.Entry.comparingByValue())
-                                        .collect(Collectors.toMap(
-                                                Map.Entry::getKey,
-                                                Map.Entry::getValue,
-                                                (e1, e2) -> e1,
-                                                LinkedHashMap::new
-                                        ))
-                        )
-                );
-        List<Circuit> circuits = new ArrayList<>();
-        Point3 box = null;
-        Point3 box2 = null;
-        while (data.values().stream().anyMatch(m -> !m.isEmpty())) {
-            Point3 hasNearest = list.stream()
-                    .filter(p -> !data.get(p).isEmpty())
-                    .min(Comparator.comparingDouble(p -> nearestDistance(p, data)))
-                    .orElseThrow();
-            Point3 unconnected = nearestPoint(hasNearest, data);
-            removeNearestPoint(hasNearest, data);
-            Circuit c1 = circuits.stream()
-                    .filter(c -> c.hasPoint(hasNearest))
-                    .findFirst()
-                    .orElse(null);
-            Circuit c2 = circuits.stream()
-                    .filter(c -> c.hasPoint(unconnected))
-                    .findFirst()
-                    .orElse(null);
-            if (c1 == null && c2 == null) {
-                // Neither in circuit - create new
-                circuits.add(new Circuit(hasNearest, unconnected));
-            } else if (c1 != null && c2 != null) {
-                // Both in circuits - merge if different
-                if (c1 != c2) {
-                    c1.merge(c2);
-                    circuits.remove(c2);
-                }
-                // else: already connected, skip
-            } else if (c1 != null) {
-                // Only hasNearest in circuit
-                c1.addPoint(unconnected);
-            } else {
-                // Only unconnected in circuit
-                c2.addPoint(hasNearest);
-            }
+    private LinkedHashMap<Point3, Double> buildSortedDistances(Point3 source, List<Point3> allPoints) {
+        return allPoints.stream()
+                .filter(not(source::equals))
+                .collect(Collectors.toMap(Function.identity(), target -> target.computeDistance(source)))
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+    }
 
-            if(circuits.size() == 1 && circuits.get(0).size() == list.size()) {
-                box = hasNearest;
-                box2 = unconnected;
-                break;
-            }
+    private void connectPoints(Point3 p1, Point3 p2, List<Circuit> circuits) {
+        Circuit c1 = findCircuit(p1, circuits);
+        Circuit c2 = findCircuit(p2, circuits);
+
+        if (c1 == null && c2 == null) {
+            circuits.add(new Circuit(p1, p2));
+        } else if (c1 != null && c2 != null && c1 != c2) {
+            c1.merge(c2);
+            circuits.remove(c2);
+        } else if (c1 != null) {
+            c1.addPoint(p2);
+        } else {
+            c2.addPoint(p1);
         }
+    }
 
-        return String.valueOf(box.x() * box2.x());
+    private Circuit findCircuit(Point3 point, List<Circuit> circuits) {
+        return circuits.stream()
+                .filter(c -> c.hasPoint(point))
+                .findFirst()
+                .orElse(null);
     }
 
     static class Circuit {
-        Circuit(Point3 p1,  Point3 p2) {
+
+        Circuit(Point3 p1, Point3 p2) {
             this.points = new HashSet<>();
             this.points.add(p1);
             this.points.add(p2);
         }
+
         final Set<Point3> points;
 
         boolean hasPoint(Point3 other) {
